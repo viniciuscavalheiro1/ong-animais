24c25ca6e50c3ea04f681f6f6c7415c45ce858d2
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-11-27 01:27:09 -0300

    [chore] classe Gato

---
+++ src/br/com/dio/OngAnimais.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class OngAnimais {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Hiroshima", "Cinza escuro", 1);
+        System.out.println(gato);
+    }
+}
